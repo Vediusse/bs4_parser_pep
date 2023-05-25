5946997540f9ccb5187fa77791261fc839816e72
Valerii Rublev <vedius.rublev@gmail.com>
2023-05-25 20:19:21 +0300

    parser-pep-flake8-2

--- src/configs.py
+++ src/configs.py
@@ -10,10 +10,20 @@ DT_FORMAT = "%d.%m.%Y %H:%M:%S"
 
 def configure_argument_parser(available_modes):
     parser = argparse.ArgumentParser(description="Парсер документации Python")
-    parser.add_argument("mode", choices=available_modes, help="Режимы работы парсера")
-    parser.add_argument("-c", "--clear-cache", action="store_true", help="Очистка кеша")
+    parser.add_argument("mode",
+                        choices=available_modes,
+                        help="Режимы работы парсера"
+                        )
+    parser.add_argument("-c",
+                        "--clear-cache",
+                        action="store_true",
+                        help="Очистка кеша"
+                        )
     parser.add_argument(
-        "-p", "--pretty", action="store_true", help="Вывод в формате PrettyTable"
+        "-p",
+        "--pretty",
+        action="store_true",
+        help="Вывод в формате PrettyTable"
     )
     parser.add_argument(
         "-o",
